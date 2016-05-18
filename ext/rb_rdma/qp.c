767cb53dfa5cf00048a0a17932517019f95e1e59
Hiroyuki Sato <hiroysato@gmail.com>
2016-05-18 17:18:10 +0900

    Hold ruby objects in QP class

--- ext/rb_rdma/qp.c
+++ ext/rb_rdma/qp.c
@@ -4,29 +4,44 @@
 
 VALUE cQP;
 
-struct rdma_qp {
+struct rb_rdma_data_qp {
+  VALUE send_cq;
+  VALUE recv_cq;
+  VALUE srq;
+  VALUE pd;
   struct ibv_qp *qp;
   struct ibv_qp_init_attr *init_attr;
 };
 
 static size_t
 memsize_rdma_qp(const void *p){
-  return sizeof(struct rema_qp *);
+  return sizeof(struct rb_rdma_data_qp);
 };
 
 static void
 free_rdma_qp(void *ptr){
-  struct rdma_qp *qp = ptr;
+  struct rb_rdma_data_qp *qp = ptr;
 
-  ibv_destroy_qp(qp->qp);
+  if( qp->qp ){
+    ibv_destroy_qp(qp->qp);
+  }
   xfree(qp->init_attr);
   xfree(qp);
 };
+static void
+mark_rdma_qp(void *ptr){
+  struct rb_rdma_data_qp *data_qp = ptr;
+
+  rb_gc_mark(data_qp->pd);
+  rb_gc_mark(data_qp->recv_cq);
+  rb_gc_mark(data_qp->send_cq);
+  rb_gc_mark(data_qp->srq);
+}
 
 const rb_data_type_t rdma_qp_type = {
   "rdma_qp",
   {
-    0, 
+    mark_rdma_qp, 
     free_rdma_qp,
     memsize_rdma_qp
   },
@@ -38,9 +53,10 @@ static VALUE
 qp_s_alloc(VALUE klass){
   VALUE obj;
 
-  struct rdma_qp *qp = ALLOC(struct rdma_qp);
+  struct rb_rdma_data_qp *qp = ALLOC(struct rb_rdma_data_qp);
   struct ibv_qp_init_attr *init_attr = ALLOC(struct ibv_qp_init_attr);
   qp->init_attr = init_attr;
+  qp->qp = NULL;
    
   obj = TypedData_Wrap_Struct(klass,&rdma_qp_type,qp);
   return obj;
@@ -48,14 +64,14 @@ qp_s_alloc(VALUE klass){
 
 static int
 get_qp_attr(VALUE self,enum ibv_qp_attr_mask attr_mask,struct ibv_qp_attr *attr){
-  struct rdma_qp *qp;
-  TypedData_Get_Struct(self,struct rdma_qp,&rdma_qp_type,qp);
+  struct rb_rdma_data_qp *qp;
+  TypedData_Get_Struct(self,struct rb_rdma_data_qp,&rdma_qp_type,qp);
   return ibv_query_qp(qp->qp,attr,attr_mask,qp->init_attr);
 }
 
 /*
 static void
-init_qp(rdma_qp *qp,enum ibv_qp_type qt_type,ibv_cq *send_cq, ibv_cq *recv_cq, 
+init_qp(rb_rdma_data_qp *qp,enum ibv_qp_type qt_type,ibv_cq *send_cq, ibv_cq *recv_cq, 
         ibv_srq *srq, int sq_sig_all, struct ibv_qp_cap *cap){
   qp->init-attr
   qp->init_attr.send_cq = send_cq;
@@ -72,12 +88,12 @@ init_qp(rdma_qp *qp,enum ibv_qp_type qt_type,ibv_cq *send_cq, ibv_cq *recv_cq,
 
 static VALUE
 rdma_qp_initialize(int argc, VALUE *argv, VALUE self){
-  struct rdma_qp *qp;
+  struct rb_rdma_data_qp *data_qp;
   int qp_type;
-  struct ibv_cq *send_cq;
-  struct ibv_cq *recv_cq;
-  struct ibv_srq *srq;
-  struct ibv_pd *pd;
+  struct rb_rdma_data_cq *send_cq;
+  struct rb_rdma_data_cq *recv_cq;
+  struct ibv_srq *srq; /* FIXME */
+  struct rb_rdma_data_pd *data_pd;
   struct ibv_qp_init_attr attr2;
   VALUE rb_pd,rb_qt_type, rb_send_cq, rb_recv_cq, rb_srq, rb_sq_sig_all, rb_cap;
 //  struct ibv_qp_init_attr init_attr;
@@ -88,35 +104,39 @@ rdma_qp_initialize(int argc, VALUE *argv, VALUE self){
 
   qp_type = NUM2INT(rb_qt_type);
 
-  TypedData_Get_Struct(rb_send_cq,struct ibv_cq,&rdma_cq_type,send_cq);
-  TypedData_Get_Struct(rb_recv_cq,struct ibv_cq,&rdma_cq_type,recv_cq);
-  TypedData_Get_Struct(rb_pd,struct ibv_pd,&rdma_pd_type,pd);
+  TypedData_Get_Struct(rb_pd,struct rb_rdma_data_pd,&rdma_pd_type,data_pd);
+  TypedData_Get_Struct(rb_send_cq,struct rb_rdma_data_cq,&rdma_cq_type,send_cq);
+  TypedData_Get_Struct(rb_recv_cq,struct rb_rdma_data_cq,&rdma_cq_type,recv_cq);
   
-                                 
-  TypedData_Get_Struct(self,struct rdma_qp,&rdma_qp_type,qp);
+  TypedData_Get_Struct(self,struct rb_rdma_data_qp,&rdma_qp_type,data_qp);
 
 #if 0
-  qp->init_attr.send_cq = send_cq;
-  qp->init_attr.recv_cq = recv_cq;
+//  qp->init_attr.send_cq = send_cq->cq;
+//  qp->init_attr.recv_cq = recv_cq->cq;
+  qp->init_attr.send_cq = cq;
+  qp->init_attr.recv_cq = cq;
   qp->init_attr.cap.max_send_wr = 1;
   qp->init_attr.cap.max_send_wr = 1;
   qp->init_attr.cap.max_recv_wr = 1;
   qp->init_attr.cap.max_send_sge = 1;
   qp->init_attr.cap.max_recv_sge = 1;
   qp->init_attr.qp_type = qp_type;
+//  qp->init_attr.qp_type = IBV_QPT_RC;
 #endif
+
+printf("***************\n");
   
-  qp->init_attr->send_cq = send_cq;
-  qp->init_attr->recv_cq = recv_cq;
-  qp->init_attr->cap.max_send_wr = 1;
-  qp->init_attr->cap.max_recv_wr = 1;
-  qp->init_attr->cap.max_send_sge = 1;
-  qp->init_attr->cap.max_recv_sge = 1;
-  qp->init_attr->qp_type = qp_type;
+  data_qp->init_attr->send_cq = send_cq->cq;
+  data_qp->init_attr->recv_cq = recv_cq->cq;
+  data_qp->init_attr->cap.max_send_wr = 1;
+  data_qp->init_attr->cap.max_recv_wr = 1;
+  data_qp->init_attr->cap.max_send_sge = 1;
+  data_qp->init_attr->cap.max_recv_sge = 1;
+  data_qp->init_attr->qp_type = qp_type;
 
 //#if 0
-  attr2.send_cq = send_cq;
-  attr2.recv_cq = recv_cq;
+  attr2.send_cq = send_cq->cq;
+  attr2.recv_cq = recv_cq->cq;
   attr2.cap.max_send_wr = 1;
   attr2.cap.max_recv_wr = 1;
   attr2.cap.max_send_sge = 1;
@@ -124,17 +144,19 @@ rdma_qp_initialize(int argc, VALUE *argv, VALUE self){
   attr2.qp_type = IBV_QPT_RC;
 //#endif
   
-//  printf("%d\n",qp->init_attr->cap.max_recv_sge = 1);
+//  printf("%d\n",data_qp->init_attr->cap.max_recv_sge = 1);
 
-  printf("pd %p\n",pd);
-  printf("send_cq %p\n",send_cq);
-  printf("recv_cq %p\n",recv_cq);
+  printf("pd %p\n",data_pd->pd);
+  printf("send_cq %p\n",send_cq->cq);
+  printf("recv_cq %p\n",recv_cq->cq);
 printf("test\n");
-  qp->qp = ibv_create_qp(pd,qp->init_attr);
-//  qp->qp = ibv_create_qp(pd,&attr2);
+  data_qp->qp = ibv_create_qp(data_pd->pd,data_qp->init_attr);
+printf("******\n");
+//  data_qp->qp = ibv_create_qp(data_pd->pd,&attr2);
 
+  printf("context in pd: %p",data_pd->pd->context);
 printf("test2\n");
-  if(!qp->qp){
+  if(!data_qp->qp){
     // TODO ERROR
     rb_exc_raise(rb_syserr_new(errno, "qp alloc fail"));
   }
@@ -148,7 +170,7 @@ printf("test2\n");
       .qp_access_flags = 0
     };
 
-    if(ibv_modify_qp(qp->qp,&attr,
+    if(ibv_modify_qp(data_qp->qp,&attr,
                      IBV_QP_STATE   |
                      IBV_QP_PKEY_INDEX |
                      IBV_QP_PORT    |
@@ -156,6 +178,7 @@ printf("test2\n");
       rb_exc_raise(rb_syserr_new(errno, "Failed to modify QP to INIT"));
     }
   }
+  printf("oK\n");
 
   return self;
 }
@@ -166,9 +189,9 @@ printf("test2\n");
 static VALUE
 attr_qp_state(VALUE self){
   struct ibv_qp_attr attr;
-  struct rdma_qp *qp;
+  struct rb_rdma_data_qp *qp;
   
-  TypedData_Get_Struct(self,struct rdma_qp,&rdma_qp_type,qp);
+  TypedData_Get_Struct(self,struct rb_rdma_data_qp,&rdma_qp_type,qp);
   ibv_query_qp(qp->qp,&attr,IBV_QP_STATE,qp->init_attr);
 
   return INT2NUM(attr.qp_state);
